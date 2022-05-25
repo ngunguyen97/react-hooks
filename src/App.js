0c52ce17aaa7e95aade11032e069ca259141ce91
lucynguyen <ngu.nguyen@kyanon.digital>
2022-05-25 18:06:29 +0700

    Input checkbox field using the useState hook

--- src/App.js
+++ src/App.js
@@ -8,24 +8,29 @@ const courses = [
 ];
 
 function App() {
-  const [checked, setChecked] = useState(-1);
+  const [checkedList, setCheckedList] = useState([]);
 
-  console.log(checked);
+  console.log(checkedList);
 
   const handleSubmit = () => {
     // Call API
-    console.log({ id: checked });
+    console.log({ ids: checkedList });
+  };
+
+  const handleAddItem = (id) => {
+    if (!checkedList.includes(id)) {
+      setCheckedList((preState) => [...preState, id]);
+    } else {
+      const newCheckedList = checkedList.filter((item) => item !== id);
+      setCheckedList(newCheckedList);
+    }
   };
 
   return (
     <div className="App">
       {courses.map((course) => (
         <div key={course.id}>
-          <input
-            type="radio"
-            onChange={() => setChecked(course.id)}
-            checked={checked === course.id}
-          />
+          <input type="checkbox" onChange={() => handleAddItem(course.id)} />
           <label>{course.name}</label>
         </div>
       ))}
