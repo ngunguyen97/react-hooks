e5fa0f5b6faa495afef35b260770602c07954fb0
lucynguyen <ngu.nguyen@kyanon.digital>
2022-05-26 11:52:18 +0700

    Todos using useState hook

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
-const courses = [
-  { id: 1, name: 'HTML' },
-  { id: 2, name: 'Javascript' },
-  { id: 3, name: 'ReactJS' },
-];
-
 function App() {
-  const [checkedList, setCheckedList] = useState([]);
-
-  console.log(checkedList);
+  const todoInputRef = useRef(null);
+  const [todoInput, setTodoInput] = useState('');
+  const [todos, setTodos] = useState(() => JSON.parse(localStorage.getItem('todos')) ?? []);
 
   const handleSubmit = () => {
-    // Call API
-    console.log({ ids: checkedList });
-  };
-
-  const handleAddItem = (id) => {
-    if (!checkedList.includes(id)) {
-      setCheckedList((preState) => [...preState, id]);
-    } else {
-      const newCheckedList = checkedList.filter((item) => item !== id);
-      setCheckedList(newCheckedList);
-    }
+    setTodos((preState) => {
+      const newTodos = [...preState, todoInput];
+      //Save todos on localStorage.
+      localStorage.setItem('todos', JSON.stringify(newTodos));
+      return newTodos;
+    });
+    setTodoInput('');
+    todoInputRef.current.focus();
   };
 
   return (
     <div className="App">
-      {courses.map((course) => (
-        <div key={course.id}>
-          <input type="checkbox" onChange={() => handleAddItem(course.id)} />
-          <label>{course.name}</label>
-        </div>
+      <input
+        ref={todoInputRef}
+        type="text"
+        placeholder="Enter a todo"
+        value={todoInput}
+        onChange={(e) => setTodoInput(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      {todos.map((todo, index) => (
+        <h1 key={index}>{todo}</h1>
       ))}
-
-      <button onClick={handleSubmit}>Get reward</button>
     </div>
   );
 }
