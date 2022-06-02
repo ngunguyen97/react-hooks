07128bad788a6fa74bc61b9a9fc51fc2cfa6ec25
lucynguyen <ngu.nguyen@kyanon.digital>
2022-06-02 09:33:09 +0700

    Todos using useEffect with dependencies

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import { useRef, useState } from 'react';
 import './App.css';
+import Content from './Content';
 
 function App() {
-  const todoInputRef = useRef(null);
-  const [todoInput, setTodoInput] = useState('');
-  const [todos, setTodos] = useState(() => JSON.parse(localStorage.getItem('todos')) ?? []);
-
-  const handleSubmit = () => {
-    setTodos((preState) => {
-      const newTodos = [...preState, todoInput];
-      //Save todos on localStorage.
-      localStorage.setItem('todos', JSON.stringify(newTodos));
-      return newTodos;
-    });
-    setTodoInput('');
-    todoInputRef.current.focus();
-  };
-
   return (
     <div className="App">
-      <input
-        ref={todoInputRef}
-        type="text"
-        placeholder="Enter a todo"
-        value={todoInput}
-        onChange={(e) => setTodoInput(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Add</button>
-      {todos.map((todo, index) => (
-        <h1 key={index}>{todo}</h1>
-      ))}
+      <Content />
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,38 @@
-import Paragraph from './Paragraph';
-
 /**
  * memo: Luu cac gia tri qua mot bien ben ngoai.
  * Su dung useCallback phai di kem voi memo
  */
 
+import { useEffect, useState } from 'react';
+const tabs = ['posts', 'comments', 'albums'];
+
 function Content() {
+  const [posts, setPosts] = useState([]);
+  const [type, setType] = useState('posts');
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/${type}`);
+      const postsJson = await response.json();
+
+      setPosts(postsJson);
+    };
+    fetchPosts().catch(console.err);
+  }, [type]);
+
   return (
     <>
       <h1>Content Section</h1>
-      <Paragraph />
+      {tabs.map((tab) => (
+        <button key={tab} onClick={() => setType(tab)}>
+          {tab}
+        </button>
+      ))}
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title ?? post.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
