e93ec2b6d47d40dc2cf9217b12f6ec1a3f34ba7e
lucynguyen <ngu.nguyen@kyanon.digital>
2022-06-08 16:53:47 +0700

    Using useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import { useState, useCallback, memo } from 'react';
+import { useState, useMemo } from 'react';
 import './App.css';
-import Content from './Content';
-import Count from './Count';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [items, setItems] = useState([]);
 
-  const addTodo = useCallback(() => {
-    setTodos((t) => [...t, 'New Todo']);
-  }, []);
+  const addItem = () => {
+    setItems((preState) => [...preState, { name, price }]);
+    setName('');
+    setPrice('');
+  };
+
+  const total = useMemo(
+    () =>
+      items.reduce((acculumatedValue, currentValue) => {
+        console.log('re-render');
+        return acculumatedValue + currentValue.price * 1;
+      }, 0),
+    [items]
+  );
 
   return (
     <>
-      <Content todos={todos} addTodo={addTodo} />
-      <hr />
-      <div>
-        <Count />
+      <div className="App">
+        <input
+          value={name}
+          type="text"
+          placeholder="Enter a name"
+          onChange={(e) => setName(e.target.value)}
+        />{' '}
+        <br />
+        <input
+          value={price}
+          type="text"
+          placeholder="Enter an amount"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <br />
+        <button onClick={addItem}>Add</button>
+        <hr />
+        <p>Total: {total}</p>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.name} - {item.price}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
